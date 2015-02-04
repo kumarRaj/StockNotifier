79412a569fad36c7b071137000ac28f6ca9e09b8
Raj Kumar <rajkum@thoughtworks.com>
2015-02-04 18:16:19 +0530

    Showing random data in table layout

---
+++ app/src/main/java/rajkum/tw/com/stocknotifier/ListStockActivity.java
@@ -0,0 +1,74 @@
+package rajkum.tw.com.stocknotifier;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+
+public class ListStockActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_stock);
+
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.listStocks);
+        String stockData[][] = {{"Tata","100","up"},{"Raja","90","down"},{"Sharan","900","upp"}};
+
+        for(String[] stock : stockData){
+            TableRow row = new TableRow(this);
+            row.setId(Integer.parseInt(stock[1]));
+
+
+            TextView name = new TextView(this);
+            name.setText(stock[0]);
+            TextView price = new TextView(this);
+            price.setText(stock[1]);
+
+            row.addView(name);
+            row.addView(price);
+
+            row.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+
+            tableLayout.addView(row, new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.FILL_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list_stock, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
